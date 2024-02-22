9a4a63315f490515531d8569c294e5cc36859861
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2024-02-22 13:23:45 +0100

    Fix RPC URL

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -20,19 +20,22 @@ const {
   ETHERSCAN_API_KEY,
 } = process.env
 const goerliRpcUrl = ALCHEMY_GOERLI_URL ?? ""
-const sepoliaRpcUrl =
-  SEPOLIA_RPC_URL ?? ALCHEMY_KEY
-    ? `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`
-    : ""
+const sepoliaRpcUrl = SEPOLIA_RPC_URL
+  ? SEPOLIA_RPC_URL
+  : ALCHEMY_KEY
+  ? `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`
+  : ""
 const scrollRpcUrl = SCROLL_RPC_URL ?? ""
-const arbitrumSepoliaRpcUrl =
-  ARBITRUM_SEPOLIA_RPC_URL ?? ALCHEMY_KEY
-    ? `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`
-    : ""
-const arbitrumRpcUrl =
-  ARBITRUM_RPC_URL ?? ALCHEMY_KEY
-    ? `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`
-    : ""
+const arbitrumSepoliaRpcUrl = ARBITRUM_SEPOLIA_RPC_URL
+  ? ARBITRUM_SEPOLIA_RPC_URL
+  : ALCHEMY_KEY
+  ? `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`
+  : ""
+const arbitrumRpcUrl = ARBITRUM_RPC_URL
+  ? ARBITRUM_RPC_URL
+  : ALCHEMY_KEY
+  ? `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`
+  : ""
 const deployerPrivateKey =
   PRIVATE_KEY ??
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
